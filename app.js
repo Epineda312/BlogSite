6e530a8ff85cef8933345cb3b6991bc8159fbe2e
Eric <Epineda773@protonmail.com>
2020-02-19 03:00:04 -0600

    complete challenge 2

--- app.js
+++ app.js
@@ -15,8 +15,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+
+app.post("/", function(req,res){
+  c
+});
+
 app.get("/", function(req, res) {
-  res.render("home");
+  res.render("home",{home: homeStartingContent});
 });
 
 
