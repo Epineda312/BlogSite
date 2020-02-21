c3aeb8d4956258986cb34a1a20ec09c051cd642d
Eric <Epineda773@protonmail.com>
2020-02-20 20:43:16 -0600

    bootstrap formatting compose page forms

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.get("/compose", function(req, res) {
 app.post("/compose", function(req, res){
   console.log(req.body.postTitle);
 });
+
 /*
 app.post("/compose", function(req, res){
   const item = req.body.postTitle;
